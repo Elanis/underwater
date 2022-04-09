fff45cd9f500c4d6d07354c59d73f7b760ebe46f
Elanis <elanis@hotmail.com>
2022-04-09 23:57:20 +0200

    Ground floor

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Submarine: <a href="https://www.flaticon.com/free-icons/ocean" title="ocean icons">Ocean icons created by Freepik - Flaticon</a>
+Torpedoe: <a href="https://www.flaticon.com/free-icons/bomb" title="bomb icons">Bomb icons created by Freepik - Flaticon</a>
--- src/App.js
+++ src/App.js
@@ -7,30 +7,86 @@ import useWindowDimensions from './hooks/useWindowDimensions';
 
 import './App.css';
 
+const GROUND_SRC = 'img/fishTile_003.png';
 const SUBMARINE_SRC = 'img/submarine.png';
+const TORPEDO_SRC = 'img/torpedo.png';
 
 preloadImages([
+	GROUND_SRC,
 	SUBMARINE_SRC,
+	TORPEDO_SRC,
 ]);
+
+let lastUpdate = Date.now();
 
 export default function App() {
 	const { width, height } = useWindowDimensions();
 	const [submarineY, setSubmarineY] = useState(0);
+	const [projectiles, setProjectiles] = useState([]);
 
-	useSubmarineControls(submarineY, setSubmarineY, height);
+	const subColumn = -width * 0.4;
+
+	useSubmarineControls(
+		submarineY, setSubmarineY,
+		height, subColumn,
+		projectiles, setProjectiles
+	);
 
 	const elements = [
 		new CanvasImage({
 			id: 'submarine',
-			x: -width * 0.4,
+			x: subColumn,
 			y: submarineY,
 			width: 75,
 			height: 75,
 			zIndex: 10,
 			draggable: false,
 			src: SUBMARINE_SRC
-		})
+		}),
+		...projectiles.map((elt, i) =>
+			new CanvasImage({
+				id: `torp-${i}`,
+				x: elt.x,
+				y: elt.y,
+				width: 25,
+				height: 25,
+				zIndex: 15,
+				draggable: false,
+				src: TORPEDO_SRC
+			})
+		),
+		...Array.from({length: 100}, (elt, i) =>
+			new CanvasImage({
+				id: `ground-${i}`,
+				x: (-1.5 * width) + 50 * i - ((Date.now() / 25) % 50),
+				y: height / 2 - 50,
+				width: 50,
+				height: 50,
+				zIndex: 15,
+				draggable: false,
+				src: GROUND_SRC
+			})
+		)
 	];
+
+	const render = () => {
+		if((Date.now() - lastUpdate) < 30) {
+			return;
+		}
+
+		lastUpdate = Date.now();
+
+		setProjectiles(projectiles.map((elt) => {
+			elt.x += 5;
+			return elt;
+		}).filter((elt) => elt.x < width * 1.5));
+
+		window.requestAnimationFrame(render);
+	};
+
+	window.requestAnimationFrame(render);
+
+	console.log(elements);
 
 	return (
 		<Canvas2D
