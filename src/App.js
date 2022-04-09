5d5208f48e2d711931cadc62fe6ac2151ed0b032
Elanis <elanis@hotmail.com>
2022-04-09 22:02:39 +0200

    Move submarine

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { Canvas2D, CanvasImage, Circle, Polygon, Rect, preloadImages } from 'canvas2d-wrapper'
 
 import useWindowDimensions from './hooks/useWindowDimensions';
@@ -10,14 +12,46 @@ preloadImages([
 	SUBMARINE_SRC,
 ]);
 
+let lastEvent = Date.now();
+
+let previousFnOnEvent = null;
+
 export default function App() {
 	const { width, height } = useWindowDimensions();
+	const [submarineY, setSubmarineY] = useState(0);
+
+	useEffect(() => {
+		const onChange = (e) => {
+			if((Date.now() - lastEvent) < 25) {
+				return;
+			}
+
+			const deltaYFromCenter = height * 0.45;
+
+			switch(e.key) {
+				case 'ArrowUp':
+					setSubmarineY(Math.max(-deltaYFromCenter, submarineY - 10));
+					break;
+				case 'ArrowDown':
+					setSubmarineY(Math.min(deltaYFromCenter, submarineY + 10));
+					break;
+			}
+
+			lastEvent = Date.now();
+		};
+
+		if(previousFnOnEvent !== null) {
+			document.removeEventListener('keydown', previousFnOnEvent);
+		}
+		document.addEventListener('keydown', onChange);
+		previousFnOnEvent = onChange;
+	}, [submarineY]);
 
 	const elements = [
 		new CanvasImage({
 			id: 'submarine',
-			x: -width * 0.35,
-			y: 0,
+			x: -width * 0.4,
+			y: submarineY,
 			width: 75,
 			height: 75,
 			zIndex: 10,
@@ -25,8 +59,6 @@ export default function App() {
 			src: SUBMARINE_SRC
 		})
 	];
-
-	console.log(elements);
 
 	return (
 		<Canvas2D
