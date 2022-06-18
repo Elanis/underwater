f1c3a030c1824c0726a62b91bff2cddbf743325c
Elanis <elanis@hotmail.com>
2022-06-18 11:16:31 +0200

    New controls for gamepad

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -7,9 +7,19 @@ export default function Controls({ quit }) {
 				<ul>
 					<li><h1>Controls</h1></li>
 
+					<li><br/></li>
+
+					<li><h2>Keyboard</h2></li>
 					<li><img src="./img/keyboard/arrow_keys.png" /> Control submarine</li>
 					<li><br/></li>
 					<li><img src="./img/keyboard/spacebar.png" /> Shoot torpedoes</li>
+
+					<li><br/></li>
+					<li><br/></li>
+
+					<li><h2>Gamepad</h2></li>
+					<li>Left joystick - Control submarine</li>
+					<li>Right buttons (X / Square) - Shoot torpedoes</li>
 
 					<li id="credits-backToMenu"><a href="#menu" onClick={quit}>Back to menu</a></li>
 				</ul>
